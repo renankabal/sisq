737ccfacbb322b4fd2a120cf16dfb2d2106e6d29
RenanKabal <renan@proesc.com>
2014-08-29 19:28:49 -0300

    Criado arquivo DB de conexão com o banco de dados

---
+++ classes/DB.class.php
@@ -0,0 +1,18 @@
+<?php
+
+	class DB
+	{
+		public function conectar(){
+			// Falta melhorar esssa parte
+			// $host="locahost";
+			// $dbname="sisq";
+			// $user="sisq";
+			// $pass="sisq@sistemap2014";
+
+			$conexao=pg_connect("host=localhost dbname=sisq user=sisq password=sisq@sistemap2014");
+
+	   		return $conexao;
+
+		}	
+
+	}
