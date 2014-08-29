f4057849d16116a41004778c8d6ebb25f890cf15
RenanKabal <renan@proesc.com>
2014-08-29 18:46:26 -0300

    Adicionado arquivo inicial do sistema

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SISQ</title>
+	<!-- <link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css' /> -->
+	<link rel="stylesheet" type="text/css" href="estilos/style.css">
+</head>
+
+<body>
+<div id="cadastrar"><a href="cadastro.php" title="Clique aqui para se cadastrar">Cadastre-se &raquo;</a></div>
+<div id="login" class="form bradius">
+<center><img src="imagens/logo.png"></center>
+	<div class="message"></div>
+	<div class="acomodar">
+		<form action="" method="POST">
+		<label for="email">Login </label><input id="email" type="text" class="txt bradius" name="email" value="" autofocus required /><br />
+		<label for="senha">Senha </label><input id="senha" type="password" class="txt bradius" name="senha" value="" required /><br />
+		<input type="submit" class="sb bradius" value="Entrar" />
+		</form>
+		<!-- acomodar -->
+	</div>
+	<!-- fechamento login -->
+</div>
+
+</body>
+</html>
