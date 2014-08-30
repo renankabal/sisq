c367a8715a8db7b8282b515dc3186a7e8b14731d
RenanKabal <renan@proesc.com>
2014-08-30 14:05:28 -0300

    Criado pasta de templates do sistema para reaproveitamento de códigos

---
+++ templates/cadastro.php
@@ -0,0 +1,24 @@
+<?php
+
+$page="Cadastro";
+include("header.php");
+?>
+
+<div id="cadastrar"><a href="index.php" title="Faça login">Login &raquo;</a></div>
+<div id="login" class="form bradius" style="top: 100px">
+<center><img src="imagens/cadastrar.png"></center>
+	<div class="message bradius" style="<?php echo $display; ?>"><?php echo $msg; ?></div>
+	<div class="acomodar">
+		<form action="cadastro.php?acao=cadastrar" method="POST">
+		<label for="nome">Nome </label><input id="nome" onkeyup="up(this)" type="text" class="txt bradius" name="nome" autofocus /><br />
+		<label for="email">Login </label><input id="email" onkeyup="down(this)" type="text" class="txt bradius" name="email" /><br />
+		<label for="senha">Senha </label><input id="senha" type="password" class="txt bradius" name="senha" /><br />
+		<input type="submit" class="sb bradius" value="Cadastrar" />
+		</form>
+		<!-- acomodar -->
+	</div>
+	<!-- fechamento login -->
+</div>
+
+</body>
+</html>
---
+++ templates/home.php
@@ -0,0 +1,22 @@
+<?php
+
+$page="PAINEL ADM";
+include("header.php");
+?>
+<!-- Botão de cadastrar novo usuário -->
+<div id="cadastrar"><a href="index.php?acao=logout" title="Clique aqui para sair do sistema">Logout &raquo;</a></div>
+<!-- Div do menu de logar -->
+<div id="login" class="form bradius">
+<!-- Imagem do sistema -->
+<center><img src="imagens/logo.png"></center>
+	<div class="message"  style="<?php echo $display; ?>"></div>
+	<div class="acomodar">
+	<h1>Seja bem Vindo!</h1>
+	<!-- acomodar -->
+	</div>
+	<br />
+	<!-- fechamento login -->
+<h6><center>copyright ©</center></h6>
+</div>
+</body>
+</html>
