42186a38bb589cf9381c9b19e04e1ecf2b8656f1
RenanKabal <renan@proesc.com>
2014-08-30 13:15:22 -0300

    Adicionado javascript para corrigir na hora de salvar os dados no banco de dados

--- cadastro.php
+++ cadastro.php
@@ -5,8 +5,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $title; ?></title>
+	<title><?php echo $title; ?></title>	
+	<link rel="shortcut icon" href="<?php echo $favicon; ?>" />
 	<link rel="stylesheet" type="text/css" href="estilos/style.css">
+
+	<script type="text/JavaScript">
+	function up(lstr){ // converte minusculas em maiusculas
+		var str=lstr.value; //obtem o valor
+		lstr.value=str.toUpperCase(); //converte as strings e retorna ao campo
+	}
+	function down(lstr){ // converte maiusculas em minusculas
+		var str=lstr.value; //obtem o valor
+		lstr.value=str.toLowerCase(); //converte as strings e retorna ao campo
+	}
+	</script>
+
 </head>
 <body>
 
@@ -16,8 +29,8 @@
 	<div class="message bradius" style="<?php echo $display; ?>"><?php echo $msg; ?></div>
 	<div class="acomodar">
 		<form action="?acao=cadastrar" method="POST">
-		<label for="nome">Nome </label><input id="nome" type="text" class="txt bradius" name="nome" autofocus /><br />
-		<label for="email">Login </label><input id="email" type="text" class="txt bradius" name="email" /><br />
+		<label for="nome">Nome </label><input id="nome" onkeyup="up(this)" type="text" class="txt bradius" name="nome" autofocus /><br />
+		<label for="email">Login </label><input id="email" onkeyup="down(this)" type="text" class="txt bradius" name="email" /><br />
 		<label for="senha">Senha </label><input id="senha" type="password" class="txt bradius" name="senha" /><br />
 		<input type="submit" class="sb bradius" value="Cadastrar" />
 		</form>
