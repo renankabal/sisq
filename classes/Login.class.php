21268c3f4a0245d49cb036078500ef2b9c866640
RenanKabal <renan@proesc.com>
2014-08-30 13:12:23 -0300

    Corrigido mensagem flash

---
+++ classes/Login.class.php
@@ -0,0 +1,24 @@
+<?php
+
+class Login{
+
+	public function logar($email, $senha){
+
+		$buscar=pg_query("SELECT * FROM usuarios WHERE email='$email' AND senha='$senha' AND status=1");
+
+		if(pg_num_rows($buscar)!=0){
+			$dados=pg_fetch_array($buscar);
+			$_SESSION["email"] = $dados["email"];
+			$_SESSION["senha"] = $dados["senha"];
+			$_SESSION["nivel"] = $dados["nivel"];
+			setcookie("Logado", 1);
+			$log=1;
+		}
+			if(isset($log)){
+				$flash="Voce foi logado com sucesso";
+			}else{
+				$flash="Usuario nao encontrado!";
+			}
+			echo $flash;
+	}
+}
