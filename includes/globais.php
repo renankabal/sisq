4e0b158ea0e61806a869629802c08d02c7985956
RenanKabal <renan@proesc.com>
2014-08-30 16:34:47 -0300

    Criado variáveis globais do usuário

--- includes/globais.php
+++ includes/globais.php
@@ -4,6 +4,11 @@ $title="SISQ";
 $acao="";
 $startaction="";
 $msg="";
+$idatual=$_SESSION["id"];
+$pessoaatual=$_SESSION["nome"];
+$emailatual=$_SESSION["email"];
+$nivelatual=$_SESSION["nivel"];
+$statusatual=$_SESSION["status"];
 $favicon="imagens/logo.ico";
 if(isset($_GET["acao"])){
 	$acao = $_GET["acao"];
