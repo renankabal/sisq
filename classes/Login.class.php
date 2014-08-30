c69af655431f4ce2321dfcf163fb57b371cbb125
RenanKabal <renan@proesc.com>
2014-08-30 15:32:05 -0300

    Retirado condição de nivel do select e adicioanado dentro de um IF

--- classes/Login.class.php
+++ classes/Login.class.php
@@ -4,20 +4,27 @@ class Login{
 
 	public function logar($email, $senha){
 
-		$buscar=pg_query("SELECT * FROM usuarios WHERE email='$email' AND senha='$senha' AND status=1");
+		$buscar=pg_query("SELECT * FROM usuarios WHERE email='$email' AND senha='$senha'");
 
 		if(pg_num_rows($buscar)!=0){
 			$dados=pg_fetch_array($buscar);
-			$_SESSION["email"] = $dados["email"];
-			$_SESSION["senha"] = $dados["senha"];
-			$_SESSION["nivel"] = $dados["nivel"];
-			setcookie("logado", 1);
-			$log=1;
+
+			if ($dados["status"] == 1){
+				$_SESSION["email"] = $dados["email"];
+				$_SESSION["senha"] = $dados["senha"];
+				$_SESSION["nivel"] = $dados["nivel"];
+				setcookie("logado", 1);
+				$log=1;
+			}else{
+				$flash="Aguarde a aprovação do ADMINISTRADOR";
+			}
 		}
 			if(isset($log)){
 				$flash="Voce foi logado com sucesso";
 			}else{
+				if(empty($flash)){
 				$flash="Usuario nao encontrado!";
+				}
 			}
 			echo $flash;
 	}
