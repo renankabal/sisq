578069f743d5dfa0705342b37be279f4096826d4
RenanKabal <renan@proesc.com>
2014-08-29 21:10:05 -0300

    Criado acao para o cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,11 @@
+<?php
+	//Inclui o cabeçalho de conexão com o banco de dados 
+	include("includes/header.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SISQ</title>
+	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" type="text/css" href="estilos/style.css">
 </head>
 <body>
@@ -9,9 +13,9 @@
 <div id="cadastrar"><a href="index.php" title="Faça login">Login &raquo;</a></div>
 <div id="login" class="form bradius" style="top: 100px">
 <center><img src="imagens/cadastrar.png"></center>
-	<div class="message"></div>
+	<div class="message bradius" style="<?php echo $display; ?>"><?php echo $msg; ?></div>
 	<div class="acomodar">
-		<form action="" method="POST">
+		<form action="?acao=cadastrar" method="POST">
 		<label for="nome">Nome </label><input id="nome" type="text" class="txt bradius" name="nome" autofocus required/><br />
 		<label for="email">Login </label><input id="email" type="text" class="txt bradius" name="email" required/><br />
 		<label for="senha">Senha </label><input id="senha" type="password" class="txt bradius" name="senha" required/><br />
