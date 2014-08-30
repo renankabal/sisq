1f203f06af3cc4caba3d2e609c0d630185c753fb
RenanKabal <renan@proesc.com>
2014-08-29 21:37:27 -0300

    Adicionado variaveis globais ao cadastro

--- cadastro.php
+++ cadastro.php
@@ -16,9 +16,9 @@
 	<div class="message bradius" style="<?php echo $display; ?>"><?php echo $msg; ?></div>
 	<div class="acomodar">
 		<form action="?acao=cadastrar" method="POST">
-		<label for="nome">Nome </label><input id="nome" type="text" class="txt bradius" name="nome" autofocus required/><br />
-		<label for="email">Login </label><input id="email" type="text" class="txt bradius" name="email" required/><br />
-		<label for="senha">Senha </label><input id="senha" type="password" class="txt bradius" name="senha" required/><br />
+		<label for="nome">Nome </label><input id="nome" type="text" class="txt bradius" name="nome" autofocus /><br />
+		<label for="email">Login </label><input id="email" type="text" class="txt bradius" name="email" /><br />
+		<label for="senha">Senha </label><input id="senha" type="password" class="txt bradius" name="senha" /><br />
 		<input type="submit" class="sb bradius" value="Cadastrar" />
 		</form>
 		<!-- acomodar -->
