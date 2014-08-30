745813251b123c28fb7d739f00643d060e104ef5
RenanKabal <renan@proesc.com>
2014-08-30 15:48:37 -0300

    Criado acao Bloquear e Aprovar

--- templates/home.php
+++ templates/home.php
@@ -8,15 +8,55 @@ include("header.php");
 <!-- Div do menu de logar -->
 <div id="login" class="form bradius">
 <!-- Imagem do sistema -->
-<center><img src="imagens/logo.png"></center>
-	<div class="message"  style="<?php echo $display; ?>"></div>
-	<div class="acomodar">
-	<h1>Seja bem Vindo!</h1>
+<!-- <center><img src="imagens/logo.png"></center> -->
+	<!-- <div class="message"  style="<?php echo $display; ?>"></div> -->
+	<!-- <div class="acomodar"> -->
+	<?php
+		if($nivel == 2){ ?>
+
+		<table width="100%" border="1">
+			<tr>
+				<th>Nome</th>
+				<th>Status</th>
+				<th>Ação</th>
+			</tr>	
+			<?php
+				$buscarusuarios=pg_query("SELECT * FROM usuarios WHERE nivel=1 ORDER BY id DESC");
+				if (pg_num_rows($buscarusuarios) == 0){
+					echo "Nenhum Usuario Cadastrado no Sistema";	
+				}else{
+					while($linha=pg_fetch_array($buscarusuarios)){
+			?>
+			<tr>
+				<th><?php echo $linha["nome"]; ?></th>
+				<th><?php echo $linha["status"]; ?></th>
+				<th><?php
+					$id = $linha["id"];
+					if ($linha["status"] == 0){
+							echo "<a href='index.php?acao=aprovar&amp;id=$id'>Aprovar</a>";
+						}else{
+							echo "<a href='index.php?acao=bloquear&amp;id=$id'>Bloquear</a>";
+						}
+					?></th>
+			</tr>
+			<?php 
+					}
+				}	
+			?>
+		</table>
+	<?php
+		}else{ ?>
+		<center>
+		<h1>Usuário Comum</h1>
+		<h1>Conteúdo do formulario aqui</h1>
+		</center>
+	<?php	
+		}
+	?>
 	<!-- acomodar -->
-	</div>
+	<!-- </div> -->
 	<br />
 	<!-- fechamento login -->
-<h6><center>copyright ©</center></h6>
 </div>
 </body>
 </html>
