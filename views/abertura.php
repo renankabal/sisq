86726facc89e371a1fb097be36af9692b0554910
RenanKabal <renan@proesc.com>
2014-08-30 16:45:18 -0300

    Criado view de abertura do sistema

---
+++ views/abertura.php
@@ -0,0 +1,21 @@
+<center><img src="imagens/imagem2.png"><br>	
+			
+<table border=0 width="60%" align="center">
+    <tr>
+        <td>
+            <font size=3 text-align=justify>
+            Temos o grande prazer de receber você <b><?php echo $pessoaatual; ?></b>
+            A empresa Proesc Soluções em TI, criada no final de 2008, tem como atividade predominante o desenvolvimento de Software para governo eletrônico.<br>
+            <b>Negócio:</b>Soluções em Tecnologia da informação.<br>
+            <b>Missão:</b>Promover o desenvolvimento de soluções tecnológicas inovadoras, visando a agilidade na gestão da informação com responsabilidade social.<br>
+            <b>Visão:</b>Ser referência nacional no desenvolvimento de soluções tecnológicas de gestão, até 2018.<br>
+            <b>Estamos sempre Buscando excelencia nos nossos produtos e por isso contamos com a sua ajuda e com sua opiniões</b></font><p><br></p>
+        </td>
+    </tr>
+</table>
+
+<center>
+    <a href="" title="Clique aqui para iniciar questionário de avaliação">
+        <input type="button" value="INICIAR AVALIAÇÃO">
+    </a>
+<center>
